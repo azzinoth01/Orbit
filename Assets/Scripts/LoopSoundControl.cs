314167283587d113244d8704786c825e83f1d087
azzinoth01 <markusdullnig@hotmail.com>
2022-01-28 12:17:49 +0100

    sound

--- Assets/Scripts/LoopSoundControl.cs
+++ Assets/Scripts/LoopSoundControl.cs
@@ -10,24 +10,59 @@ public class LoopSoundControl : MonoBehaviour
     public AudioSource loopAudio;
 
     private bool transitionPlayed;
+    private bool isPlaying;
+
+    public bool IsPlaying {
+        get {
+            return isPlaying;
+        }
+
+
+    }
+
 
     // Start is called before the first frame update
     void Start() {
-        startAudio.Play();
-        transitionPlayed = false;
+
+        startPlaying();
+
+
+
     }
 
     // Update is called once per frame
     void Update() {
 
-        if (transitionPlayed == false && startAudio.isPlaying == false && transitionAudio.isPlaying == false && loopAudio.isPlaying == false) {
-            transitionAudio.Play();
-            transitionPlayed = true;
-        }
-        else if (transitionPlayed == true && startAudio.isPlaying == false && transitionAudio.isPlaying == false && loopAudio.isPlaying == false) {
-            loopAudio.Play();
+        if (transitionAudio != null) {
+            if (transitionPlayed == false && startAudio.isPlaying == false && transitionAudio.isPlaying == false && loopAudio.isPlaying == false) {
+                transitionAudio.Play();
+                transitionPlayed = true;
+                Debug.Log("transition started");
+            }
+            else if (loopAudio != null) {
+                if (transitionPlayed == true && startAudio.isPlaying == false && transitionAudio.isPlaying == false && loopAudio.isPlaying == false) {
+                    loopAudio.Play();
+                    Debug.Log("loop started");
+                }
+            }
         }
 
 
+
+
+
+    }
+
+    public void startPlaying() {
+        startAudio.Play();
+        transitionPlayed = false;
+        isPlaying = true;
+    }
+
+    public void stopPlaying() {
+        isPlaying = false;
+        startAudio.Stop();
+        transitionAudio.Stop();
+        loopAudio.Stop();
     }
 }
